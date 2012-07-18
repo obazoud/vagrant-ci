b05febb6ea7a96c433700d40059d2d15111c82f3
Olivier Bazoud <olivier.bazoud@gmail.com>
2012-07-18 10:00:39 +0200

    Check if lastest version of chef is not installed

--- update_chef.sh
+++ update_chef.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-time gem install chef
+if [ ! -d '/opt/vagrant_ruby/lib/ruby/gems/1.8/gems/chef-10.12.0' ]; then
+  echo "Updating Chef Solo..."
+  sudo apt-get install make --yes
+  time gem install -no-rdoc --no-ri chef
+else
+  echo "Skipping Chef Solo update."
+fi
 
